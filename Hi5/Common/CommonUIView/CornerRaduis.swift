816ced831185b25e361e92c0fcb7d15dd8892e9b
AhmedibnAdam <ibn.abuadam@gmail.com>
2019-12-26 14:42:02 +0200

    first commit

---
+++ Hi5/Common/CommonUIView/CornerRaduis.swift
@@ -0,0 +1,24 @@
+//
+//  CornerRaduis.swift
+//  Hi5
+//
+//  Created by Adam on 12/26/19.
+//  Copyright © 2019 FudexApp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CreateCornerRauis {
+    
+    static func viewRaduis(view:UIView , number: CGFloat)-> UIView{
+        view.layer.cornerRadius = number
+        return view
+    }
+    
+    static func ButtonRaduis(button:UIButton , number: CGFloat)-> UIButton{
+        button.layer.cornerRadius = number
+        return button
+    }
+}

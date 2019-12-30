3de2581e00410dbf3e3e4cd33dd0726ae6f0a592
AhmedibnAdam <ibn.abuadam@gmail.com>
2019-12-30 17:27:49 +0200

    first commit

---
+++ Hi5/APIError.swift
@@ -0,0 +1,9 @@
+//
+//  APIError.swift
+//  Hi5
+//
+//  Created by Adam on 12/28/19.
+//  Copyright © 2019 FudexApp. All rights reserved.
+//
+
+import Foundation
---
+++ Hi5/Common/CommonUIView/ShowAlertView.swift
@@ -0,0 +1,9 @@
+//
+//  ShowAlertView.swift
+//  Hi5
+//
+//  Created by Adam on 12/26/19.
+//  Copyright © 2019 FudexApp. All rights reserved.
+//
+
+import Foundation
